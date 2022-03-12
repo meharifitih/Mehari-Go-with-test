7d5d6a3e306f9b96ac3020c1102eb45a560b81f5
Mehari <74234292+meharifitih@users.noreply.github.com>
2022-03-12 09:46:13 +0300

    updating the arry into a slice and updating the function sum to accept slice rather than array

--- Arrays/sum.go
+++ Arrays/sum.go
@@ -1,12 +1,20 @@
 package main
 
-func sum(numbers [5]int) int {
+func sum(numbers []int) int {
 	sum := 0
 	for _, number := range numbers {
 		sum += number
 	}
 	return sum
 }
+
+// func Sum(numbers []int) int {
+// 	sum := 0
+// 	for _, number := range numbers {
+// 		sum += number
+// 	}
+// 	return sum
+// }
 
 func main() {
 	// fmt.Println(sum([5]int{1, 2, 3, 4, 5}))
