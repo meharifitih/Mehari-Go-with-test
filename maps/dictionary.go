085458f2990d4cbdd9c2ca6f8adef9cb16f982ab
Mehari <74234292+meharifitih@users.noreply.github.com>
2022-03-12 19:50:28 +0300

    created a test function for adding a map key and value

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,5 +1,23 @@
 package main
 
+import "errors"
+
 func Search(dictionary map[string]string, word string) string {
 	return dictionary[word]
+}
+
+type Dictionary map[string]string
+
+var ErrNotFound = errors.New("could not find the word you were looking for")
+
+func (d Dictionary) Search(word string) (string, error) {
+	defination, ok := d[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+	return defination, nil
+}
+
+func (d Dictionary) Add(word, defination string) {
+	d[word] = defination
 }
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -3,15 +3,49 @@ package main
 import "testing"
 
 func TestSearch(t *testing.T) {
-	dictionary := map[string]string{"test": "this is just a test"}
-	got := Search(dictionary, "test")
-	want := "this is just a test"
-	assertString(t, got, want)
+	dictionary := Dictionary{"test": "this is just a test"}
+
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
+		assertString(t, got, want)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, got := dictionary.Search("unknown")
+		assertError(t, got, ErrNotFound)
+	})
+}
+
+func assertError(t testing.TB, got, want error) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got error %q want %q", got, want)
+	}
 }
 
 func assertString(t testing.TB, got, want string) {
 	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q given, %q", got, want, "test")
+	}
+}
+
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{}
+	word := "test"
+	defination := "this is just a test"
+	dictionary.Add(word, defination)
+	assertdefination(t, dictionary, word, defination)
+}
+
+func assertdefination(t testing.TB, dictionart Dictionary, word, defination string) {
+	t.Helper()
+	got, err := dictionart.Search(word)
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+	if defination != got {
+		t.Errorf("got %q want %q", got, defination)
 	}
 }
