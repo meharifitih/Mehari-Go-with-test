88f12b6f136f4513c8d4939d8d77e61833c4c1d8
Mehari <74234292+meharifitih@users.noreply.github.com>
2022-03-12 12:52:10 +0300

    added and interface called Shapes that has a methode area that returns float 64

--- structs/shapes.go
+++ structs/shapes.go
@@ -15,11 +15,15 @@ type Circle struct {
 	Radius float64
 }
 
+type Shape interface {
+	Area() float64
+}
+
 func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
 }
 
-func Primeter(rectangle Rectangle) float64 {
+func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Width + rectangle.Height)
 }
 
